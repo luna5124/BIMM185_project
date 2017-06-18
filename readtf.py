dfb4d6db28457d9a79da98c33c2d38ac4290ced6
Lihui Lu <lunarlu5124@gmail.com>
2017-06-18 16:11:51 -0700

    add plot.R

--- readtf.py
+++ readtf.py
@@ -11,6 +11,9 @@ import pymysql
 import scipy
 from scipy import stats
 import matplotlib.pyplot as plt
+import numpy
+from numpy import linspace
+from scipy.stats.kde import gaussian_kde
 
 
 hostname = 'localhost'
@@ -37,6 +40,14 @@ def query_gene_locau_tag(conn, locus_tag):
 		return None
 	else:
 		return result
+
+def query_directons(conn):
+	cur = conn.cursor()
+	sql_statement = ("select genes.gene_id, genes.name, strand, left_position, right_position from genes inner join(select gene_id, min(left_position) as left_position, max(right_position) as right_position from exons group by gene_id) position on position.gene_id = genes.gene_id where genes.genome_id = 1 order by left_position;")
+	cur.execute(sql_statement)
+	result = cur.fetchall()
+	#print(result)
+	return result
 
 def query_synonyms(conn, gene):
 	cur=conn.cursor()
@@ -114,7 +125,7 @@ def alter_colombos(conn, attrs):
 
 def query_gene_pairs(conn):
 	cur = conn.cursor()
-	sql_statement=("select t1.gene_id, t2.gene_id from (select * from TF_gene where TF not in (select TF from (select count(*) as count, TF from TF_gene group by TF) t where count > 130)) t1, TF_gene t2 where t1.gene_id != t2.gene_id and t1.TF = t2.TF and t1.effect = t2.effect group by t1.gene_id, t2.gene_id;")
+	sql_statement=("select t1.gene_id, t2.gene_id from (select * from TF_gene where TF not in (select TF from (select count(*) as count, TF from TF_gene group by TF) t where count > 130)) t1, TF_gene t2 where t1.gene_id < t2.gene_id and t1.TF = t2.TF and t1.effect = t2.effect group by t1.gene_id, t2.gene_id;")
 	cur.execute(sql_statement)
 	result = cur.fetchall()
 	cur.close()
@@ -195,6 +206,9 @@ def read_operons(myConnection):
 	create_operon_table(myConnection)
 	operon_count = 0
 	operon_file = open("operons.txt",'w')
+	directons = query_directons(myConnection)
+	h0_files = open("h0.txt",'w')
+	h0_pairs = []
 	with open("OperonSet.txt",'r') as file:
 		for line in file:
 			if "#" in line:
@@ -224,7 +238,127 @@ def read_operons(myConnection):
 
 					if result is not None:
 						operon_file.write(str(operon_count) + '\t' + str(result[0]) + '\n')
-	
+				if strand == 'forward':
+					#first gene is the leftmost
+					for i in range(len(genes)-1):
+						operons.add((genes[i],genes[i+1]))
+
+					if genes[0] in sorted_genes:
+						gene_index = sorted_genes.index(genes[0])
+						
+						if gene_index != 0:
+
+							prev = directons[gene_index - 1]
+
+							#print(prev)
+							if prev[0] not in border_dic:
+								if prev[2] == '+':
+									distance = directons[gene_index][3] - prev[4]
+									borders.add((prev[0],genes[0]))
+									h0.append(distance)
+									border_dic[prev[0]] = genes[0]
+									border_dic[genes[0]] = prev[0]
+									print(prev[0],genes[0])
+								else:
+									print(prev[0],genes[0],'not on the same strand')
+							elif border_dic[prev[0]] != genes[0]:
+								if prev[2] == '+':
+									distance = directons[gene_index][3] - prev[4]
+									borders.add((prev[0],genes[0]))
+									h0.append(distance)
+									border_dic[prev[0]] = genes[0]
+									border_dic[genes[0]] = prev[0]
+									print(prev[0],genes[0])
+								else:
+									print(prev[0],genes[0],'not on the same strand')
+
+								#print(directons[gene_index],prev,distance)
+
+					if genes[-1] in sorted_genes:
+						gene_index = sorted_genes.index(genes[-1])
+						if gene_index != len(sorted_genes) - 1:
+							next_gene = directons[gene_index + 1]
+							borders.add((genes[-1],next_gene[0]))
+							if next_gene[0] not in border_dic:
+								if next_gene[2] == "+":
+									distance = next_gene[3] - directons[gene_index][4]
+									h0.append(distance)
+									border_dic[next_gene[0]] = genes[-1]
+									border_dic[genes[-1]] = next_gene[0]
+									print(next_gene[0],genes[-1])
+								else:
+									print(next_gene[0],genes[-1], 'not on the same strand')
+							elif border_dic[next_gene[0]] != genes[-1]:
+								if next_gene[2] == "+":
+									distance = next_gene[3] - directons[gene_index][4]
+									h0.append(distance)
+									border_dic[next_gene[0]] = genes[-1]
+									border_dic[genes[-1]] = next_gene[0]
+									print(next_gene[0],genes[-1])
+								else:
+									print(next_gene[0],genes[-1],'not on the same strand')
+								#print(directons[gene_index],next_gene,distance)
+
+
+				else:
+					for i in range(len(genes)-1,-1,-1):
+						operons.add((genes[i],genes[i-1]))
+
+					if genes[0] in sorted_genes:
+						gene_index = sorted_genes.index(genes[0])
+						if gene_index != len(sorted_genes) - 1:
+
+							next_gene = directons[gene_index + 1]
+							borders.add((genes[0],next_gene[0]))
+							if next_gene[0] not in border_dic:
+								if next_gene[2] == "-":
+									distance = next_gene[3] - directons[gene_index][4]
+									h0.append(distance)
+									border_dic[next_gene[0]] = genes[-1]
+									border_dic[genes[-1]] = next_gene[0]
+									print(next_gene[0],genes[-1])
+								else:
+									print(next_gene[0],genes[-1],'not on the same strand')
+							elif border_dic[next_gene[0]] != genes[-1]:
+								if next_gene[2] == "-":
+									distance = next_gene[3] - directons[gene_index][4]
+									h0.append(distance)
+									border_dic[next_gene[0]] = genes[-1]
+									border_dic[genes[-1]] = next_gene[0]
+									print(next_gene[0],genes[-1])
+								else:
+									print(next_gene[0],genes[-1],'not on the same strand')
+
+								#print(directons[gene_index],next_gene,distance)
+						
+
+					if genes[-1] in sorted_genes:
+						gene_index = sorted_genes.index(genes[-1])
+						if gene_index != 0:
+							prev = directons[gene_index - 1]
+							borders.add((prev[0],genes[-1]))
+							#print(prev)
+							if prev[0] not in border_dic:
+								if prev[2] == '-':
+									distance = directons[gene_index][3] - prev[4]
+									h0.append(distance)
+									border_dic[prev[0]] = genes[0]
+									border_dic[genes[0]] = prev[0]
+									print(prev[0],genes[0])
+								else:
+									print(prev[0],genes[0],'not on the same strand')
+							elif border_dic[prev[0]] != genes[-1]:
+								if prev[2] == '-':
+									distance = directons[gene_index][3] - prev[4]
+									h0.append(distance)
+									border_dic[prev[0]] = genes[0]
+									border_dic[genes[0]] = prev[0]
+									print(prev[0],genes[0])
+								else:
+									print(prev[0],genes[0],'not on the same strand')
+
+
+				
 	import_operon(myConnection)
 
 	operon_file.close()
@@ -257,6 +391,49 @@ def read_colombos(myConnection):
 	#import_colombos(myConnection)
 	#colombos_file.close()
 	return colombos
+
+def pdf_calculate(h0, h1):
+	dist_space0 = linspace(-1, 1, 1000)
+
+	kde1 = gaussian_kde(h1, 0.5)
+	#kde1.set_bandwidth(bw_method='silverman')
+	#kde1.set_bandwidth(bw_method=kde1.factor * 3.0)
+	#kde1.covariance_factor = lambda : 0.25
+	#kde1._compute_covariance()
+
+
+	density1 = kde1(dist_space0)
+	max_density1 = max(density1)
+	for i in range(len(density1)):
+		density1[i] = density1[i]/max_density1
+	plt.plot(dist_space0, kde1(dist_space0)/max(kde1(dist_space0)))
+	
+
+	kde0 = gaussian_kde(h0,0.5)
+	#kde0.set_bandwidth(bw_method=kde0.factor * 3.0)
+	#kde0.covariance_factor = lambda : 0.25
+	#kde0._compute_covariance()
+	
+	density0 = kde0(dist_space0)
+	max_density0 = max(density0)
+	dist0 = [i for i in range(min(h0),min(h0))]
+	for i in range(len(density0)):
+		density0[i] = density0[i]/max_density1
+		#print(max_density0)
+	plt.plot(dist_space0, kde0(dist_space0)/max(kde1(dist_space0)))
+	plt.show()
+	#print("max density0: ", max_density0)
+	#print(density0)
+
+
+	#print(kde0(0))
+	#print(kde0(1))
+	#print(kde1(0))
+	#print(kde1(1))
+
+	#plt.show()
+	#return kde0, kde1
+
 
 
 def main():
@@ -367,7 +544,7 @@ def main():
 
 	
 
-		#print(distance, rho)
+		 #print(distance, rho)
 		#plt.plot(distance, rho)
 	#unique_rhos = sorted(unique_rhos)
 	#plt.plot(distances, rhos, "ro")
@@ -383,6 +560,7 @@ def main():
 		ms.append(sum(means[key])/len(means[key]))
 	'''
 
+	'''
 	means = {}
 	with open("operons_rho.out", "r") as file:
 		for line in file:
@@ -399,6 +577,15 @@ def main():
 
 	plt.plot(ds, ms, "ro")
 	plt.show()
+	'''
+
+
+	pdf_calculate(distances, rhos)
+
+
+
+
+
 
 
 
